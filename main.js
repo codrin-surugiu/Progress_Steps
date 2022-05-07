9f747160ebcd130f2108b1ed8359c8657568f978
codrin-surugiu <49073617+codrin-surugiu@users.noreply.github.com>
2022-05-07 12:13:50 +0300

    Finished

---
+++ main.js
@@ -0,0 +1,70 @@
+const progress = document.getElementById("progress");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  update();
+  progressBar();
+  enablePrev();
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+  progressBar();
+  enablePrev();
+});
+
+//console.log('sal);
+
+//let afisareConsola = () => {
+// console.log('sal)
+//}
+//afisareConsola();
+
+//addEventListener('click', afisaConsola);
+
+//for(let i=0; i<arra.length; i++){
+//console.log(array[i])
+//console.log(i)
+//
+//.forEach((codrin, indexescu))
+//console.log(c)
+//console.log(index)
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+}
+
+let progressBar = () => {
+  const actives = document.querySelectorAll(".active");
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+};
+
+let enablePrev = () => {
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+    prev.disabled = false;
+  } else {
+    next.disabled = false;
+    prev.disabled = false;
+  }
+};
